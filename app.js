1ed498c978fb76dcecad64a43541a230f98fc4cf
Byron Sha <byronysha@gmail.com>
2017-02-06 22:40:56 -0800

    Setup socket.io and moves lists

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 
+const server = require('http').createServer(app)
+const port = 3000
+
+const io = require('socket.io')(server)
+
+io.on('connection', function(socket) {
+  console.log('New connection')
+
+  socket.on('move', function(move) {
+    socket.broadcast.emit('move', move)
+  })
+})
+
 app.use(express.static(__dirname))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
-
-const server = require('http').Server(app)
-const port = 3000
 
 server.listen(port, () => {
   console.log('Listening on port ' + port)
--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
+const socket = io()
+
 function initGame() {
   const config = {
     draggable:  true,
     position: 'start',
-    onDrop,
-    onSnapEnd,
+    onDrop
   }
 
   board = new ChessBoard('gameboard', config)
@@ -19,6 +20,25 @@ function onDrop(source, target) {
   if (move === null) {
     return 'snapback'
   }
+
+  updateMovesList(move)
+  socket.emit('move', move)
+}
+
+socket.on('move', function(move) {
+  game.move(move)
+  updateMovesList(move)
+
+  setTimeout(function() {
+    board.position(game.fen())
+  }, 1000)
+})
+
+function updateMovesList(move) {
+  let color = move.color === 'w' ? 'white' : 'black'
+  const listItem = `<li>${move.piece} from ${move.from} to ${move.to}</li>`
+
+  $(`#${color}-moves ul`).append(listItem)
 }
 
 // update board position after piece snap
